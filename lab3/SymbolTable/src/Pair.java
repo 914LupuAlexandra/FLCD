2260ac4b8f9fe9473f8b60c30706ede9c04f00ed
914LupuAlexandra <91674339+914LupuAlexandra@users.noreply.github.com>
2023-10-31 21:38:45 +0200

    add st

---
+++ lab3/SymbolTable/src/Pair.java
@@ -0,0 +1,25 @@
+public class Pair {
+    private Integer first;
+    private Integer second;
+
+    public Pair(Integer first, Integer second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public Integer getFirst() {
+        return first;
+    }
+
+    public Integer getSecond() {
+        return second;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
+    }
+}
