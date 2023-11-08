8e3f40903aca65c557e26be8aaad5b443cda6e6a
914LupuAlexandra <91674339+914LupuAlexandra@users.noreply.github.com>
2023-11-08 12:20:53 +0200

    add final

---
+++ lab3 - final/SymbolTable/src/HashTable.java
@@ -0,0 +1,117 @@
+import java.util.ArrayList;
+
+public class HashTable {
+    private ArrayList<ArrayList<String>> hashTable;
+    private Integer size;
+
+    public HashTable(Integer size) {
+        this.size = size;
+        this.hashTable = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            this.hashTable.add(new ArrayList<>());
+        }
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    // hash function
+    public Integer hash(String key) {
+        int sum = 0;
+        char[] characters = key.toCharArray();
+        for (char c: characters) {
+            sum += c;
+        }
+        return sum % size;
+    }
+
+    // returns a pair with the position of the element from the both arrays
+    public Pair findElemPosition(String elem) {
+        int pos = hash(elem);
+        if (!hashTable.get(pos).isEmpty()) {
+            ArrayList<String> elems = this.hashTable.get(pos);
+            for(int i = 0; i < elems.size(); i++) {
+                if (elems.get(i).equals(elem)) {
+                    return new Pair(pos, i);
+                }
+            }
+        }
+        return null;
+    }
+
+    public Boolean contains(String elem) {
+        return this.findElemPosition(elem) != null;
+    }
+
+    public Boolean add(String elem) {
+        if(contains(elem)) {
+            return false;
+        }
+        Integer pos = hash(elem);
+        ArrayList<String> elems = this.hashTable.get(pos);
+        elems.add(elem);
+        return true;
+    }
+
+    public String findByPosition (Pair<Integer, Integer> position) throws Exception{
+        if (this.hashTable.size() <= position.getFirst() || this.hashTable.get(position.getFirst()).size() <= position.getSecond()) {
+            throw new Exception("Invalid position");
+        }
+        return this.hashTable.get(position.getFirst()).get(position.getSecond());
+    }
+
+    public Boolean isEmpty() {
+       for (ArrayList<String> bucket: hashTable) {
+           if (!bucket.isEmpty()) { // at least one bucket is not empty
+               return false;
+           }
+       }
+       return true; // all buckets are empty
+    }
+
+    public int getNrElems() {
+        int count = 0;
+        for (ArrayList<String> bucket: hashTable) {
+            count += bucket.size();
+        }
+        return count;
+    }
+
+    // clears every element from the arrays
+    public void clear() {
+        for (ArrayList<String> bucket: hashTable) {
+            bucket.clear();
+        }
+    }
+
+    public Boolean remove(String elem) {
+        Pair<Integer, Integer> position = findElemPosition(elem);
+        if (position != null) {
+            int bucketIndex = position.getFirst();
+            int elemIndex = position.getSecond();
+            hashTable.get(bucketIndex).remove(elemIndex);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder computedString = new StringBuilder();
+        for(int i = 0; i < this.hashTable.size(); i++){
+            if(this.hashTable.get(i).size() > 0){
+                for(int j  = 0; j < this.hashTable.get(i).size(); j ++) {
+                    computedString.append(i);
+                    computedString.append(", ");
+                    computedString.append(j);
+                    computedString.append(" - ");
+                    computedString.append(this.hashTable.get(i));
+                    computedString.append("\n");
+                }
+
+            }
+        }
+        return computedString.toString();
+    }
+}
