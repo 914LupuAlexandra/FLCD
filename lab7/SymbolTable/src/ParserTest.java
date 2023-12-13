8580a0307244d3ba49e8f918ad30a2726728afc1
914LupuAlexandra <91674339+914LupuAlexandra@users.noreply.github.com>
2023-12-13 12:46:36 +0200

    add lab7

---
+++ lab7/SymbolTable/src/ParserTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParserTest {
+
+    @Test
+    void momentaryInsuccess() {
+
+    }
+
+    @Test
+    void success() {
+    }
+
+    @Test
+    void advance() {
+    }
+
+    @Test
+    void expand() {
+    }
+
+    @Test
+    void back() {
+    }
+
+    @Test
+    void anotherTry() {
+    }
+
+    @Test
+    void checkSequence() {
+    }
+
+    @Test
+    void checkGrammarLeftRecursive() {
+
+    }
+}
